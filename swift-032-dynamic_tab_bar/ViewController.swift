88e59caf1358867466757a3712e59c70bd30ddee
Luiz Araujo <luizcarlos_bsb2006@hotmail.com>
2021-10-06 22:07:08 -0300

    Initial Commit

---
+++ swift-032-dynamic_tab_bar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  swift-032-dynamic_tab_bar
+//
+//  Created by Luiz Carlos da Silva Araujo on 06/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
